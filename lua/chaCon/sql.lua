de412930483ea3ebf8e6e2f99344361d841fcbb1
Octavio Montt <octavio.montt@artlogic.net>
2024-09-16 11:58:54 +0000

    harpoon

--- lua/chaCon/sql.lua
+++ lua/chaCon/sql.lua
@@ -7,4 +7,9 @@ return {
 		name = "Illustratedgallery Source",
 		url = "mariadb://root@localhost:3306/s_gallery_manager_596892a05a409152007b23c7_d80293e3cdc64bf8b5571",
 	},
+	{ name = "EI-Saieh", url = "mariadb://root@localhost:3306/elsaiehgallery" },
+	{
+		name = "EI-Saieh Source",
+		url = "mariadb://root@localhost:3306/s_gallery_manager_5a4448d84cd8504b328b4569_f3a6f1debec14cc5bf98e",
+	},
 }
--- lua/plugins/file.lua
+++ lua/plugins/file.lua
@@ -91,11 +91,39 @@ return {
 
 			vim.keymap.set(
 				"n",
-				"<C-n>",
+				"<C-q>",
 				":Neotree filesystem reveal left<CR>",
 				{ desc = "Open Neo-Tree [File] explorer", silent = true }
 			)
 			vim.keymap.set("n", "<C-b>", ":Neotree buffers right<CR>", { desc = "Open Neo-Tree [Buffer] explorer" })
+		end,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+
+			harpoon:setup({
+				settings = {
+					save_on_toggle = true,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<C-p>", function()
+				harpoon:list():prev()
+			end)
+			vim.keymap.set("n", "<C-n>", function()
+				harpoon:list():next()
+			end)
 		end,
 	},
 }
