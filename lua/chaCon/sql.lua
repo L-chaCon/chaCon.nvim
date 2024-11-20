f4c544221b4c6dc93c275322973e8093a385f13b
Octavio Montt <octavio.montt@artlogic.net>
2024-11-20 14:21:54 +0000

    sql trabajando ahora

--- lua/chaCon/sql.lua
+++ lua/chaCon/sql.lua
@@ -2,14 +2,12 @@ return {
 	{ name = "Artlogic", url = "mariadb://root@localhost:3306/" },
 	{ name = "Jackshainman Source", url = "mariadb://root@localhost:3306/jackshainman_source" },
 	{ name = "Jackshainman", url = "mariadb://root@localhost:3306/jackshainman" },
-	{ name = "Illustratedgallery", url = "mariadb://root@localhost:3306/illustratedgallery" },
+	{ name = "Van De Weghe", url = "mariadb://root@localhost:3306/vandeweghe" },
+	{ name = "Van De Weghe Source", url = "mariadb://root@localhost:3306/vandeweghe_source" },
 	{
-		name = "Illustratedgallery Source",
-		url = "mariadb://root@localhost:3306/s_gallery_manager_596892a05a409152007b23c7_d80293e3cdc64bf8b5571",
+		name = "API CMS sqlite",
+		url = "sqlite:/home/artlogic/workspace/imports/ArtlogicImportsCMSAPI/artlogicimportscmsapi/site.db",
 	},
-	{ name = "EI-Saieh", url = "mariadb://root@localhost:3306/elsaiehgallery" },
-	{
-		name = "EI-Saieh Source",
-		url = "mariadb://root@localhost:3306/s_gallery_manager_5a4448d84cd8504b328b4569_f3a6f1debec14cc5bf98e",
-	},
+	{ name = "Akzonobel Art Foundation Source", url = "mariadb://root@localhost:3306/akzonobelfoundation_source" },
+	{ name = "Akzonobel Art Foundation", url = "mariadb://root@localhost:3306/akzonobelartfoundation" },
 }
