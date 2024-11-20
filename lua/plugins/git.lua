f425b6f46ed10b6ceebdf45a50ecdd60cfd92d91
chaCon <montt.octavio@gmail.com>
2024-11-20 14:06:52 +0000

    limpieza de git para que sea mas comodo

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,39 +8,22 @@ return {
 			})
 
 			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", { desc = "[G]it [P]review Changes" })
-			vim.keymap.set("n", "<leader>gB", "<cmd>Gitsigns blame<cr>", { desc = "[G]it [B]lame" })
+			vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns blame<cr>", { desc = "[G]it [B]lame" })
 			vim.keymap.set(
 				"n",
-				"<leader>gb",
+				"<leader>gB",
 				"<cmd>Gitsigns toggle_current_line_blame<cr>",
-				{ desc = "Disable line [G]it [B]lame" }
+				{ desc = "Disabl  line [G]it [B]lame" }
 			)
 			vim.keymap.set(
 				"n",
-				"<leader>gtl",
+				"<leader>gl",
 				"<cmd>Gitsigns toggle_linehl<cr>",
-				{ desc = "[G]it [T]oggle [L]ine highlight" }
+				{ desc = "[G]it toggle [L]ine highlight" }
 			)
-			vim.keymap.set(
-				"n",
-				"<leader>gtd",
-				"<cmd>Gitsigns toggle_deleted<cr>",
-				{ desc = "[G]it [T]oggle [D]eleted" }
-			)
-			vim.keymap.set(
-				"n",
-				"<leader>gaf",
-				"<cmd>Gitsigns stage_buffer<cr>",
-				{ desc = "[G]it [A]dd current [F]ile" }
-			)
-			vim.keymap.set(
-				"n",
-				"<leader>gah",
-				"<cmd>Gitsigns stage_buffer<cr>",
-				{ desc = "[G]it [A]dd current [H]unk" }
-			)
-			vim.keymap.set("n", "<leader>ghn", "<cmd>Gitsigns next_hunk<cr>", { desc = "[G]it next [H]unk" })
-			vim.keymap.set("n", "<leader>ghN", "<cmd>Gitsigns prev_hunk<cr>", { desc = "[G]it previous [H]unk" })
+			vim.keymap.set("n", "<leader>gd", "<cmd>Gitsigns toggle_deleted<cr>", { desc = "[G]it toggle [D]eleted" })
+			vim.keymap.set("n", "<leader>gn", "<cmd>Gitsigns next_hunk<cr>", { desc = "[G]it next [H]unk" })
+			vim.keymap.set("n", "<leader>gN", "<cmd>Gitsigns prev_hunk<cr>", { desc = "[G]it previous [H]unk" })
 		end,
 	},
 }
