ca5e2e7a08784d89742e964658d0d9a1e0c86da9
octavio-montt-artlogic <octavio.montt@artlogic.net>
2024-12-06 11:05:57 +0000

    treesitter to big of a file

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,10 @@
+print("HELLO")
+
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
 		---@diagnostic disable-next-line: missing-fields
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
@@ -11,7 +12,6 @@ return {
 				"html",
 				"lua",
 				"markdown",
-				"markdown_inline",
 				"vim",
 				"vimdoc",
 				"yaml",
@@ -21,7 +21,16 @@ return {
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
-			highlight = { enable = true },
+			highlight = {
+				enable = true,
+				disable = function(lang, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+			},
 			indent = { enable = true },
 		})
 
