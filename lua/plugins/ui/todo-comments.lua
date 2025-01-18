d697b54ad8693e9d6b10a12eaad2022e6342a45b
Octavio Montt <montt.octavio@gmail.com>
2025-01-18 19:26:39 +0100

    tails added to todo

--- lua/plugins/ui/todo-comments.lua
+++ lua/plugins/ui/todo-comments.lua
@@ -1,22 +1,22 @@
 return {
-	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			signs = true,
-			keywords = { chaCon = { icon = "䷁", color = "chaCon" } },
-			colors = { chaCon = { "#25BE6A" } },
-		},
-		keys = {
-			{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "[S]earch [T]odo" },
-			{ "<leader>xt", "<cmd>Trouble todo<cr>", desc = "Trouble todo" },
-		},
-	},
-	{
-		"brenoprata10/nvim-highlight-colors",
-		config = function()
-			require("nvim-highlight-colors").setup({})
-		end,
-	},
+  {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      signs = true,
+      keywords = { chaCon = { icon = "䷁", color = "chaCon" }, TAILS = { icon = "󰩃", color = "tails" } },
+      colors = { chaCon = { "#25BE6A" }, tails = { "#029ffa" } },
+    },
+    keys = {
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "[S]earch [T]odo" },
+      { "<leader>xt", "<cmd>Trouble todo<cr>", desc = "Trouble todo" },
+    },
+  },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      require("nvim-highlight-colors").setup({})
+    end,
+  },
 }
