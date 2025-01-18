26e774dad2cdd5ccc924b83b2b9714e47d6848c2
Octavio Montt <montt.octavio@gmail.com>
2025-01-18 13:40:47 +0100

    formatter fixed, just lenguajes that I use and 2 indentation for lua

--- lua/plugins/formatter/conform.lua
+++ lua/plugins/formatter/conform.lua
@@ -1,47 +1,49 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				-- Customize or remove this keymap to your liking
-				"<leader>=",
-				function()
-					require("conform").format({ async = true })
-				end,
-				mode = "",
-				desc = "Format buffer",
-			},
-		},
-		-- Everything in opts will be passed to setup()
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        "<leader>=",
+        function()
+          require("conform").format({ async = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    -- Everything in opts will be passed to setup()
 
-		---@module "conform"
-		---@type conform.setupOpts
-		opts = {
-			-- Define your formatters
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "ruff_format" },
-				-- markdown = { "mdformat" },
-				-- go = { "goimports", "gofmt" },
-				yaml = { "yamlfmt" },
-			},
-			default_format_opts = {
-				lsp_format = "fallback",
-			},
-			-- Set up format-on-save
-			format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
-			-- Customize formatters
-			formatters = {
-				shfmt = {
-					prepend_args = { "-i", "4" },
-				},
-			},
-		},
-		init = function()
-			-- If you want the formatexpr, here is the place to set it
-			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		end,
-	},
+    -- This will provide type hinting with LuaLS
+    ---@module "conform"
+    ---@diagnostic disable-next-line: undefined-doc-name
+    ---@type conform.setupOpts
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_format" },
+      },
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+      -- Customize formatters
+      formatters = {
+        stylua = {
+          prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+        },
+        shfmt = {
+          prepend_args = { "-i", "4" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
 }
