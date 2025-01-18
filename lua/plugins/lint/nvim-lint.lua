b0a7fe07d7f29e5f3db014b08d00f4d03898ec5c
Octavio Montt <montt.octavio@gmail.com>
2025-01-18 13:45:18 +0100

    lint fix for tails

--- lua/plugins/lint/nvim-lint.lua
+++ lua/plugins/lint/nvim-lint.lua
@@ -1,41 +1,19 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = "BufWritePost",
-	config = function()
-		require("lint").linters_by_ft = {
-			python = {
-				"mypy",
-			},
-			go = {
-				"golangcilint",
-			},
-			yaml = {
-				"ansible_lint",
-			},
-		}
-		-- correr al guardar
-		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-			group = vim.api.nvim_create_augroup("PythonLint", { clear = true }),
-			pattern = { "*.py" },
-			callback = function()
-				require("lint").try_lint()
-				-- require("lint").try_lint("pylint") -- NOTE: This is only for extreme verbose lint
-			end,
-		})
-		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-			group = vim.api.nvim_create_augroup("GoLint", { clear = true }),
-			pattern = { "*.go" },
-			callback = function()
-				print("ACUERDATE DE CONFIGURRAR EL LINTER THE GO")
-			end,
-		})
-		-- FIX: NEED TO MAKE THIS AUTOMATIC (OR ON OFF) FOR ANSIBLE
-		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-			group = vim.api.nvim_create_augroup("Ansible", { clear = true }),
-			pattern = { "*.yml", "*.yaml" },
-			callback = function()
-				require("lint").try_lint()
-			end,
-		})
-	end,
+  "mfussenegger/nvim-lint",
+  event = "BufWritePost",
+  config = function()
+    require("lint").linters_by_ft = {
+      python = {
+        "ruff",
+      },
+    }
+    -- correr al guardar
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      group = vim.api.nvim_create_augroup("PythonLint", { clear = true }),
+      pattern = { "*.py" },
+      callback = function()
+        require("lint").try_lint()
+      end,
+    })
+  end,
 }
