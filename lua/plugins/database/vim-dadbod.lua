18190ce1650fa8ef406d1fd88a2fd03a6099fc80
Octavio Montt <montt.octavio@gmail.com>
2025-01-23 16:21:49 +0100

    database form enviroment file

--- lua/plugins/database/vim-dadbod.lua
+++ lua/plugins/database/vim-dadbod.lua
@@ -1,19 +1,27 @@
 return {
-	"kristijanhusak/vim-dadbod-ui",
-	dependencies = {
-		{ "tpope/vim-dadbod", lazy = true },
-		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-	},
-	cmd = {
-		"DBUI",
-		"DBUIToggle",
-		"DBUIAddConnection",
-		"DBUIFindBuffer",
-	},
-	init = function()
-		-- Your DBUI configuration
-		vim.g.db_ui_use_nerd_fonts = 1
-		vim.keymap.set("n", "<leader>S", ":DBUI<CR>", { desc = "SQL", silent = true })
-		vim.g.dbs = { { name = "chaCon", url = "mysql://root@localhost:3306/chacon" } }
-	end,
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    -- NOTE: My databases are loaded with dotenv
+    {
+      "tpope/vim-dotenv",
+      event = "VimEnter",
+      config = function()
+        vim.cmd(":Dotenv " .. vim.fn.stdpath("config") .. "/.databases")
+      end,
+    },
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.keymap.set("n", "<leader>S", ":DBUI<CR>", { desc = "SQL", silent = true })
+    -- vim.g.dbs = { { name = "some_name", url = "some_url" } }
+  end,
 }
