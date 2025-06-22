2b0c95c41ebcfc2d4044f372c6223a6e3447c737
Octavio Montt <montt.octavio@gmail.com>
2025-06-22 14:54:43 +0100

    not using harpoon anymore

--- lua/plugins/file-management/harpoon.lua
+++ lua/plugins/file-management/harpoon.lua
@@ -2,6 +2,7 @@ return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
+  enable = false,
   config = function()
     local harpoon = require("harpoon")
 
