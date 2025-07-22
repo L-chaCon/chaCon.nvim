8ae3c2a5be0395cd6de06ba3581a16d7a5d889e9
Octavio Montt <octavio.montt@tails.com>
2025-07-22 10:23:49 +0100

    feat: added icon picker in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,11 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-symbols.nvim",
+  },
+  keys = {
+    { "<leader>si", "<cmd>Telescope symbols<cr>", desc = "Icons" },
+  },
   opts = function(_, opts)
     local ivy_pickers = {
       "find_files",
@@ -9,9 +15,18 @@ return {
       "lsp_references",
     }
 
+    local cursor_pickers = {
+      "symbols",
+    }
+
     for _, picker in ipairs(ivy_pickers) do
       opts.pickers[picker] = opts.pickers[picker] or {}
       opts.pickers[picker].theme = "ivy"
+    end
+
+    for _, picker in ipairs(cursor_pickers) do
+      opts.pickers[picker] = opts.pickers[picker] or {}
+      opts.pickers[picker].theme = "cursor"
     end
   end,
 }
