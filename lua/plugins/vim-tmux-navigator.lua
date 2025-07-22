5184fc03aac7f733a17315ffe308cc26d5945e49
Octavio Montt <octavio.montt@tails.com>
2025-07-22 10:24:08 +0100

    feat: tmux navegation

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,10 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<C-H>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<C-J>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<C-K>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<C-L>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
