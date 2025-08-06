30f4287ae80b82cc3aceafae27242926fcda5592
Octavio Montt <octavio.montt@tails.com>
2025-08-06 22:14:49 +0100

    feat: go lsp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- import/override with your plugins
     { import = "plugins" },
   },
