5655714f413170336ce8366cc61603787cc53d40
Octavio Montt <montt.octavio@gmail.com>
2026-05-08 12:14:05 +0100

    feat: update packages

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -47,13 +47,6 @@ return {
       }),
       matching = { disallow_symbol_nonprefix_matching = false },
     })
-    -- Setup for vim-dadbod
-    cmp.setup.filetype({ "sql", "mysql", "mariadb" }, {
-      sources = {
-        { name = "vim-dadbod-completion" },
-        { name = "buffer" },
-      },
-    })
     return opts
   end,
 }
